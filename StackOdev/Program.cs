79ae870e72f782c48e27babec305f872e86ee3a0
Zeynep Yalçınkaya <130675703+Zeynep1481@users.noreply.github.com>
2023-07-19 23:48:33 +0300

    Proje dosyası ekle.

---
+++ StackOdev/Program.cs
@@ -0,0 +1,45 @@
+﻿using StackOdev.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yığının eleman sayısını giriniz: ");
+            int elemanSayisi=Convert.ToInt32(Console.ReadLine());
+            Yigin yiginOrnegi = new Yigin(elemanSayisi);
+            for(int i= 0; i<elemanSayisi; i++)
+            {
+                Console.WriteLine("Sayı giriniz: ");
+                int sayi=Convert.ToInt32(Console.ReadLine());
+                yiginOrnegi.push(sayi);
+            }
+
+             Console.WriteLine("-----");
+             yiginOrnegi.print();
+
+            Console.WriteLine("-----");
+            Console.WriteLine("Yığından çıkarmak istediğiniz sayıyı giriniz: ");
+            int cıkarılacakdata = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----");
+            yiginOrnegi.pop(cıkarılacakdata);
+            yiginOrnegi.print();
+            Console.WriteLine("-----");
+            int a=Convert.ToInt32(Console.ReadLine());
+            yiginOrnegi.pop(a);
+            //Console.WriteLine("Yığının ilk elamanı ile son elemanının yer değiştirmiş hali: ");
+           // yiginOrnegi.Degistir();
+            yiginOrnegi.print();
+
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ StackOdev/Properties/Yigin.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace StackOdev.Properties
+{
+    internal class Yigin
+    {
+        int index;
+        int boyut;
+        int[] veriler;
+        int Veriler_Length;
+        int[] cıkanVeriler;
+        int[] AraElemanlar;
+      
+
+        public Yigin(int eleman_sayisi)
+        {
+            this.boyut = eleman_sayisi;
+            this.index = -1;
+            Veriler_Length = eleman_sayisi;
+            veriler = new int[eleman_sayisi];
+            cıkanVeriler= new int[eleman_sayisi];
+            AraElemanlar= new int[eleman_sayisi];   
+
+        }
+
+        public void push(int data)
+        {
+            // yığın doluysa
+            if(this.index==this.boyut-1)
+            {
+                Console.WriteLine("Stack Dolu");
+            }
+
+            else
+            {
+                this.index++;
+                veriler[this.index] = data; 
+            }
+        }
+
+        public void pop(int cıkarılacak_data) 
+        {
+            // stack boşsa
+            if (this.index == -1)
+            {
+                Console.WriteLine("Stack Boş");
+              
+            }
+
+            else
+            {
+                int i = -1;
+                while (cıkarılacak_data != veriler[this.index])
+                {
+                    i++;
+                    cıkanVeriler[i] = peek();
+
+                    this.index--;
+
+                    if (this.index == -1)
+                    {
+                        Console.WriteLine("Aradığınız eleman yığında bulunamadı.");
+                        break;
+                    }
+                }
+                if (this.index != -1)
+                {
+                    this.index--;
+                }
+
+
+                for (int j = i; j > -1; j--)
+                {
+                    push(cıkanVeriler[j]);
+                }
+
+            }
+        }
+        
+       
+
+        public int peek()
+        {
+            return veriler[this.index]; 
+        }
+
+
+        public void Degistir()
+        {
+
+            if (this.index == (boyut - 2))
+            {
+                Veriler_Length--;
+
+            }
+
+            int sonEleman=peek();
+            this.index--;
+            int a = 0;
+
+           
+
+
+            for ( a = 0; a<((Veriler_Length)-2); a++)
+            {
+                AraElemanlar[a]=peek();
+                this.index--;
+            }
+
+            int ilkEleman=peek();
+            this.index--;
+
+
+            push(sonEleman);
+            for (int b = (a-1); b > -1; b--)
+            {
+                push(AraElemanlar[b]);
+            }
+            push(ilkEleman);
+
+
+        }
+
+        public void print()
+        {
+            if (this.index == -1)
+            {
+                Console.WriteLine("Stack Boş");
+            }
+            else
+            {
+                for (int i = this.index; i > -1; i--)
+                {
+                    Console.WriteLine(this.veriler[i]);
+                }
+            }
+
+        }
+
+    }
+}
